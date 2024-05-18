ee8aa73cd512f9e90f11b3bed6a925dcfb4ae71e
kseble <seble.temesgenk@gmail.com>
2024-05-18 09:36:47 +0000

    debug_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixes a filename extension typo
-exec {'rename file':
- command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
- path => '/bin/:/sbin/:/usr/sbin/',
+
+exec {'fix-wordpress':
+ command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+ path    => '/usr/local/bin/:/bin/'
 }
