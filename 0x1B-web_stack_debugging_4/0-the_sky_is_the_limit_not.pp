018807c1e0436bb9667cd5b5263c83e510db3ad3
kseble <seble.temesgenk@gmail.com>
2024-05-18 11:32:13 +0000

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,15 +2,15 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
- # Modify the ULIMIT value
- command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
- # Specify the path for sed command
- path    => '/usr/local/bin/:/bin',
+# Modify the ULIMIT value
+command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+# Specify the path for sed command
+path    => '/usr/local/bin/:/bin',
 }
 # Restart Nginx
 exec { 'nginx-restart':
- # Restart Nginx service
- command => '/etc/init.d/nginx restart',
- # Specify the path for the init.d script
- path    => '/etc/init.d/',
+# Restart Nginx service
+command => '/etc/init.d/nginx restart',
+# Specify the path for the init.d script
+path    => '/etc/init.d/',
 }
